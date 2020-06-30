9acfb14a1b3cc023ba7f39800f8958c9faffe87a
MikelAlilaj <alilajmikel@gmail.com>
2020-06-30 16:02:22 -0700

    Multi-Auth-System for User and for Admin

---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Admin;
+use Illuminate\Support\Facades\Auth;
+
+class AdminController extends Controller
+{
+
+    public function __construct()
+    {
+        $this->middleware('auth:admin');
+    }
+
+    public function index(){
+        return view('admin.home');
+    }
+
+    public function Logout(){
+        Auth::logout();
+        $notification = array(
+            'message'=>'Admin Successfully Logout',
+            'alert-type'=>'success'
+        );
+        return redirect()->route('admin.login')->with($notification);
+    }
+}
---
+++ resources/views/admin/home.blade.php
@@ -0,0 +1,14 @@
+@extends('admin.admin_layouts')
+
+@section('admin_content')
+
+    <div class="container"><br><br><br><br>
+        <center>
+            <h1>Admin Home page</h1>
+        </center>
+
+    </div>
+
+
+
+@endsection
